18d0b119efd871c9f3e21092c1fba817fd42bc10
Hussain Hussain <hhussain@know-center.at>
2020-09-22 03:32:16 +0200

    added OGB datasets

--- structack/compare_to_baselines.py
+++ structack/compare_to_baselines.py
@@ -10,34 +10,49 @@ from deeprobust.graph.global_attack import DICE, Random, Metattack
 from structack.structack import StructackOneEnd, StructackBothEnds, StructackBothEndsGreedy
 import pandas as pd
 import time
+import os
 
 
+def postprocess_adj(adj):
+    adj = normalize_adj(adj)
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+    return adj
 
 def attack_dice(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
     model.attack(adj, labels, n_perturbations)
+    modified_adj = model.modified_adj
+    return postprocess_adj(modified_adj)
 
 def attack_random(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
     model.attack(adj, n_perturbations)
+    modified_adj = model.modified_adj
+    return postprocess_adj(modified_adj)
 
 def attack_structack1(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
     model.attack(adj, n_perturbations)
+    modified_adj = model.modified_adj
+    return postprocess_adj(modified_adj)
 
 def attack_structack2(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
     model.attack(adj, n_perturbations)
+    modified_adj = model.modified_adj
+    return postprocess_adj(modified_adj)
 
 def attack_structack2_greedy(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
     model.attack(adj, n_perturbations)
-
+    modified_adj = model.modified_adj
+    return postprocess_adj(modified_adj)
 
 def attack_mettaack(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    adj, features, labels = preprocess(adj, features, labels, preprocess_adj=False)
-    return model.attack(features, adj, labels, idx_train, idx_unlabeled, n_perturbations, ll_constraint=False)
+    model.attack(features, adj, labels, idx_train, idx_unlabeled, n_perturbations, ll_constraint=False)
+    return model.modified_adj
+
 
 def build_random(adj=None, features=None, labels=None, idx_train=None, device=None):
     return Random()
 
 def build_dice(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return DICE(),
+    return DICE()
 
 def build_structack1(adj=None, features=None, labels=None, idx_train=None, device=None):
     return StructackOneEnd(degree_percentile_range=[0,.1])
@@ -46,14 +61,11 @@ def build_structack2(adj=None, features=None, labels=None, idx_train=None, devic
     return StructackBothEnds(degree_percentile_range=[0,.1,0,.1])
 
 def build_structack2_greedy(adj=None, features=None, labels=None, idx_train=None, device=None):
-    StructackBothEndsGreedy()
+    return StructackBothEndsGreedy()
 
-def build_mettack(adj=None, features=None, labels=None, idx_train=None, device=None):
-
-    adj, features, labels = preprocess(adj, features, labels, preprocess_adj=False, device=device)
-    
+def build_mettack(adj=None, features=None, labels=None, idx_train=None, device=None):    
     lambda_ = 0
-
+    
     # Setup Surrogate Model
     surrogate = GCN(nfeat=features.shape[1], nclass=labels.max().item()+1, nhid=16,
             dropout=0.5, with_relu=False, with_bias=True, weight_decay=5e-4, device=device)
@@ -85,38 +97,37 @@ def apply_perturbation(model_builder, attack, data, ptb_rate, cuda, seed=0):
     idx_train, idx_val, idx_test = data.idx_train, data.idx_val, data.idx_test
     idx_unlabeled = np.union1d(idx_val, idx_test)
 
+
     n_perturbations = int(ptb_rate * (adj.sum()//2))
+
+    if model_builder == build_mettack:
+        adj, features, labels = preprocess(adj, features, labels, preprocess_adj=False)
 
     # build the model
     model = model_builder(adj, features, labels, idx_train, device)
-    
+        
     tick = time.time()
     # perform the attack
-    attack(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled)
+    modified_adj = attack(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled)
     elapsed = time.time() - tick
 
-    modified_adj = model.modified_adj
-
-    adj, features, labels = preprocess(adj, features, labels, preprocess_adj=False, sparse=True, device=device)
-
-    modified_adj = normalize_adj(modified_adj)
-    modified_adj = sparse_mx_to_torch_sparse_tensor(modified_adj)
     modified_adj = modified_adj.to(device)
     return modified_adj, elapsed
 
-
-def test(adj,data, cuda):
-    ''' test on GCN '''
+def pre_test_data(data,device):
     features, labels = data.features, data.labels
     idx_train, idx_val, idx_test = data.idx_train, data.idx_val, data.idx_test
     idx_unlabeled = np.union1d(idx_val, idx_test)
-
-    device = torch.device("cuda:0" if cuda else "cpu")
-
     _ , features, labels = preprocess(data.adj, features, labels, preprocess_adj=False, sparse=True, device=device)
+    return features, labels, idx_train, idx_val, idx_test
+
+def test(adj, data, cuda, data_prep,nhid=16):
+    ''' test on GCN '''
+    device = torch.device("cuda:0" if cuda else "cpu")
+    features, labels, idx_train, idx_val, idx_test = data_prep(data,device)
 
     gcn = GCN(nfeat=features.shape[1],
-              nhid=16,
+              nhid=nhid,
               nclass=labels.max().item() + 1,
               dropout=0.5, device=device)
 
@@ -130,62 +141,62 @@ def test(adj,data, cuda):
     output = gcn.output
     loss_test = F.nll_loss(output[idx_test], labels[idx_test])
     acc_test = accuracy(output[idx_test], labels[idx_test])
-    # print("Test set results:",
-    #       "loss= {:.4f}".format(loss_test.item()),
-    #       "accuracy= {:.4f}".format(acc_test.item()))
 
     return acc_test.item()
 
 
 
-
 def main():
-    df_path = 'reports/initial_eval.csv'
-    attacks = [
-        # attack_random,
-        # attack_dice,
-        # attack_structack1,
-        # attack_structack2,
-        # attack_structack2_greedy,
-        attack_mettaack,
-    ]
-    model_names = [
-        'Random',
-        'DICE',
-        'StructackOneEnd',
-        'StructackBothEnds',
-        'StructackBothEndsGreedy',
-        'Metattack',
-    ]
-    model_builders = [
-        # build_random,
-        # build_dice,
-        # build_structack1,
-        # build_structack2,
-        # build_structack2_greedy,
-        build_mettack,
-    ]
+    df_path = 'reports/initial_eval-2.csv'
     datasets = ['cora', 'cora_ml', 'citeseer', 'polblogs', 'pubmed']
-    cuda = torch.cuda.is_available()
     for dataset in datasets:
         for attack, model_builder, model_name in zip(attacks,model_builders, model_names):
             data = Dataset(root='/tmp/', name=dataset)
-            adj,_,_ = preprocess(data.adj, data.features, data.labels, preprocess_adj=False, sparse=True, device=torch.device("cuda:0" if cuda else "cpu"))
-            acc = test(adj, data, cuda)
-            row = {'dataset':dataset, 'attack':'Clean', 'seed':None, 'acc':acc}
-            print(row)
+            # adj,_,_ = preprocess(data.adj, data.features, data.labels, preprocess_adj=False, sparse=True, device=torch.device("cuda:0" if cuda else "cpu"))
+            # acc = test(adj, data, cuda, pre_test_data)
+            # row = {'dataset':dataset, 'attack':'Clean', 'seed':None, 'acc':acc}
+            # print(row)
             df = pd.DataFrame()
-            df = df.append(row, ignore_index=True)
+            # df = df.append(row, ignore_index=True)
             for perturbation_rate in [0.05,0.01,0.10,0.15,0.20]:
                 for seed in range(10):
                     modified_adj, elapsed = apply_perturbation(model_builder, attack, data, perturbation_rate, cuda, seed)
-                    acc = test(modified_adj, data, cuda)
+                    acc = test(modified_adj, data, cuda, pre_test_data)
                     row = {'dataset':dataset, 'attack':model_name, 'seed':seed, 'acc':acc, 'perturbation_rate':perturbation_rate,'elapsed':elapsed}
                     print(row)
                     df = df.append(row, ignore_index=True)
-            cdf = pd.read_csv(df_path)
+            cdf = pd.DataFrame(columns=df.columns)
+            if os.path.exists(df_path):
+                cdf = pd.read_csv(df_path)
             df = pd.concat([cdf,df])
             df.to_csv(df_path,index=False)
+
+
+attacks = [
+    attack_random,
+    attack_dice,
+    attack_structack2_greedy,
+    attack_structack1,
+    attack_structack2,
+    # attack_mettaack,
+]
+model_names = [
+    'Random',
+    'DICE',
+    'StructackBothEndsGreedy',
+    'StructackOneEnd',
+    'StructackBothEnds',
+    # 'Metattack',
+]
+model_builders = [
+    build_random,
+    build_dice,
+    build_structack2_greedy,
+    build_structack1,
+    build_structack2,
+    # build_mettack,
+]
+cuda = torch.cuda.is_available()
 
 if __name__ == '__main__':
     main()
---
+++ structack/compare_to_baselines_ogb.py
@@ -0,0 +1,98 @@
+import torch
+import numpy as np
+import torch.nn.functional as F
+import torch.optim as optim
+from deeprobust.graph.defense import GCN
+from deeprobust.graph.utils import *
+from deeprobust.graph.global_attack import DICE, Random, Metattack
+from structack.structack import StructackBothEndsGreedy
+import pandas as pd
+import time
+import os
+from ogb.nodeproppred import NodePropPredDataset
+from structack.compare_to_baselines import *
+from scipy.sparse import csr_matrix
+
+
+def apply_perturbation(model_builder, attack, data, ptb_rate, cuda, seed=0):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed(seed)
+
+
+    device = torch.device("cuda:0" if cuda else "cpu")
+
+    adj, features, labels, idx_train, idx_val, idx_test = ogb_to_deeprobust(data)
+    idx_unlabeled = np.union1d(idx_val, idx_test)
+
+
+    n_perturbations = int(ptb_rate * (adj.sum()//2))
+
+    if model_builder == build_mettack:
+        adj = sparse_mx_to_torch_sparse_tensor(adj).to(device)
+        features = torch.FloatTensor(features).to(device)
+        labels = torch.LongTensor(labels).to(device)
+
+    # build the model
+    model = model_builder(adj, features, labels, idx_train, device)
+    
+    tick = time.time()
+    # perform the attack
+    modified_adj = attack(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled)
+    elapsed = time.time() - tick
+
+    modified_adj = modified_adj.to(device)
+    return modified_adj, elapsed
+
+
+def ogb_to_deeprobust(dataset):
+    split_idx = dataset.get_idx_split()
+    train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
+    graph, label = dataset[0]
+    label = label.flatten()
+    edge_index = graph['edge_index']
+    reversed_edges = np.array([edge_index[1],edge_index[0]])
+    edge_index = np.concatenate((edge_index,reversed_edges),axis=1)
+
+    adj = csr_matrix((np.ones([edge_index.shape[1]]), edge_index))
+    adj.data = np.clip(adj.data,0,1)
+    features = graph['node_feat']
+    return adj,features,label,train_idx,valid_idx,test_idx
+    
+
+def pre_test_data_ogb(data,device):
+    adj, features, labels, idx_train, idx_val, idx_test = ogb_to_deeprobust(data)
+    adj = sparse_mx_to_torch_sparse_tensor(adj).to(device)
+    features = torch.FloatTensor(features).to(device)
+    labels = torch.LongTensor(labels).to(device)
+    return features, labels, idx_train, idx_val, idx_test
+
+def main_ogb():
+    df_path = 'reports/initial_eval-ogb.csv'
+    datasets = 'ogbn-arxiv'.split()
+    for dataset in datasets:
+        for attack, model_builder, model_name in zip(attacks,model_builders, model_names):
+            data = NodePropPredDataset(name = dataset)
+            # device = torch.device("cuda:0" if cuda else "cpu")
+            # adj = ogb_to_deeprobust(data)[0]
+            # adj = sparse_mx_to_torch_sparse_tensor(adj).to(device)
+            # print(test(adj,data, cuda, pre_test_data_ogb))
+            df = pd.DataFrame()
+            for perturbation_rate in [0.005,0.010,0.015,0.020,0.050]:
+                for seed in range(1):
+                    modified_adj, elapsed = apply_perturbation(model_builder, attack, data, perturbation_rate, cuda, seed)
+                    acc = test(modified_adj, data, cuda, pre_test_data_ogb,nhid=16)
+                    row = {'dataset':dataset, 'attack':model_name, 'seed':seed, 'acc':acc, 'perturbation_rate':perturbation_rate,'elapsed':elapsed}
+                    print(row)
+                    df = df.append(row, ignore_index=True)
+            cdf = pd.DataFrame(columns=df.columns)
+            if os.path.exists(df_path):
+                cdf = pd.read_csv(df_path)
+            df = pd.concat([cdf,df])
+            df.to_csv(df_path,index=False)
+
+
+
+if __name__ == '__main__':
+    main_ogb()
