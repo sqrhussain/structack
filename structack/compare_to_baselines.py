6aa719aec626ac3a8e2d9e761de3def792d85221
Hussain Hussain <hhussain@know-center.at>
2020-10-15 18:23:13 +0200

    fixed StructackDegree corner case

--- structack/compare_to_baselines.py
+++ structack/compare_to_baselines.py
@@ -174,9 +174,9 @@ def test(adj, data, cuda, data_prep,nhid=16):
 
 
 def main():
-    df_path = 'reports/eval/initial_eval-tmp.csv'
+    df_path = 'reports/eval/initial_eval.csv'
     datasets = ['citeseer', 'cora', 'cora_ml', 'polblogs', 'pubmed']
-    datasets = ['citeseer']
+    # datasets = ['polblogs']
     for dataset in datasets:
         for attack, model_builder, model_name in zip(attacks,model_builders, model_names):
             data = Dataset(root='/tmp/', name=dataset)
@@ -185,9 +185,9 @@ def main():
             # row = {'dataset':dataset, 'attack':'Clean', 'seed':None, 'acc':acc}
             # print(row)
             # df = df.append(row, ignore_index=True)
-            for perturbation_rate in [0.05]: #,0.01,0.10,0.15,0.20]:
+            for perturbation_rate in [0.01,0.10,0.15,0.20]:
                 for seed in range(10):
-                    modified_adj, elapsed = apply_perturbation(model_builder, attack, data, perturbation_rate, cuda, seed)
+                    modified_adj, elapsed = apply_perturbation(model_builder, attack, data, perturbation_rate, cuda and (dataset!='pubmed'), seed)
                     acc = test(modified_adj, data, cuda, pre_test_data)
                     row = {'dataset':dataset, 'attack':model_name, 'seed':seed, 'acc':acc, 'perturbation_rate':perturbation_rate,'elapsed':elapsed}
                     print(row)
@@ -203,7 +203,7 @@ attacks = [
     # attack_random,
     # attack_dice,
     # attack_structack_fold, 
-    # attack_structack_only_distance,
+    attack_structack_only_distance,
     attack_structack_distance,
     # attack_mettaack,
 ]
@@ -211,7 +211,7 @@ model_names = [
     # 'Random',
     # 'DICE',
     # 'StructackGreedyFold', # this is StructackDegree in the paper
-    # 'StructackOnlyDistance', # this is StructackDistance in the paper
+    'StructackOnlyDistance', # this is StructackDistance in the paper
     'StructackDistance', # this is Structack in the paper
     # 'Metattack',
 ]
@@ -219,7 +219,7 @@ model_builders = [
     # build_random,
     # build_dice,
     # build_structack_fold,
-    # build_structack_only_distance,
+    build_structack_only_distance,
     build_structack_distance,
     # build_mettack,
 ]
