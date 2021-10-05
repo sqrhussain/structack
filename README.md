7b2805b8d2c3cf222593e5dc6a82fceb718304ea
Hussain Hussain <hhussain@know-center.at>
2021-10-05 14:51:27 +0200

    added dependencies and updated README

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
 # structack
-Structure-aware attacks on GNNs
+Structure-aware attacks on GNNs [Paper](https://dl.acm.org/doi/abs/10.1145/3465336.3475110)
 
 This repository implements global black-box adversarial attacks on the node classification task with graph neural networks.
 The attacks have only access to the adjacency matrix and not the nodes feature vectors.
 These attacks are currenlty based on the knowledge of the degree distribution and community structure.
 
+### Run
+The main testin code is in the file `evaluate_attacks.py`.
+Please run `python -u -m evaluate_attacks` from the project root directory.
+
+* The `--model` argument chooses the GNN model to train after the perturbation (`gcn, sgc, gat`).
+
+* With the `--dataset` argument, you can list the datasets to perturb (`citeseer, cora, cora_ml, polblogs, pubmed`).
+
+* This file can run in 3 modes
+	1. Evaluation on clean graphs with the argument `--approach_type clean`
+	2. Evaluation on perturbed graphs using baseline attacks (i.e., random, DICE, Metattack and PGD) with the argument `--approach_type baseline`
+	3. Evaluation on perturbed graphs using structack combinations with the argument `--approach_type structack`
+
 ### Dependencies
-To be added
+`python >= 3.7`
+
+Please install [PyTorch](https://pytorch.org/get-started/locally/) and [PyG](https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html), then run `pip install -r requirements.txt`.
+
+### Citation
+
+```
+@inproceedings{10.1145/3465336.3475110,
+author = {Hussain, Hussain and Duricic, Tomislav and Lex, Elisabeth and Helic, Denis and Strohmaier, Markus and Kern, Roman},
+title = {Structack: Structure-Based Adversarial Attacks on Graph Neural Networks},
+year = {2021},
+isbn = {9781450385510},
+publisher = {Association for Computing Machinery},
+address = {New York, NY, USA},
+url = {https://doi.org/10.1145/3465336.3475110},
+doi = {10.1145/3465336.3475110},
+booktitle = {Proceedings of the 32nd ACM Conference on Hypertext and Social Media},
+pages = {111–120},
+numpages = {10},
+keywords = {network centrality, network similarity, adversarial attacks, graph neural networks},
+location = {Virtual Event, USA},
+series = {HT '21}
+}
+
+```
--- structack/evaluate_attacks.py
+++ structack/evaluate_attacks.py
@@ -9,12 +9,8 @@ from deeprobust.graph.utils import *
 from deeprobust.graph.data import Dataset
 from deeprobust.graph.global_attack import DICE, Random, Metattack, PGDAttack, MinMax
 from structack.structack import StructackBase, build_custom
-# from structack.structack import StructackDegreeRandomLinking, StructackDegree, StructackDegreeDistance,StructackDistance
-# from structack.structack import StructackEigenvectorCentrality, StructackBetweennessCentrality, StructackClosenessCentrality
-# from structack.structack import StructackPageRank, StructackKatzSimilarity, StructackCommunity
 import structack.node_selection as ns
 import structack.node_connection as nc
-# from structack.calc_unnoticeability import *
 import pandas as pd
 import time
 import argparse
@@ -35,37 +31,6 @@ def attack_random(model, adj, features, labels, n_perturbations, idx_train, idx_
     model.attack(adj, n_perturbations)
     modified_adj = model.modified_adj
     return postprocess_adj(modified_adj)
-
-def attack_structack1(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack2(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack2_greedy(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_fold(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_distance(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_only_distance(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
 
 def attack_structack(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
     model.attack(adj, n_perturbations)
@@ -88,77 +53,11 @@ def attack_minmax(model, adj, features, labels, n_perturbations, idx_train, idx_
     return model.modified_adj
 
 
-def attack_structack_eigenvector_centrality(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_betwenness_centrality(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_closeness_centrality(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_pagerank(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_katz_similarity(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
-def attack_structack_community(model, adj, features, labels, n_perturbations, idx_train, idx_unlabeled):
-    model.attack(adj, n_perturbations)
-    modified_adj = model.modified_adj
-    return postprocess_adj(modified_adj)
-
 def build_random(adj=None, features=None, labels=None, idx_train=None, device=None):
     return Random()
 
 def build_dice(adj=None, features=None, labels=None, idx_train=None, device=None):
     return DICE()
-
-# def build_structack1(adj=None, features=None, labels=None, idx_train=None, device=None):
-#     return StructackOneEnd(degree_percentile_range=[0,.1])
-#
-# def build_structack2(adj=None, features=None, labels=None, idx_train=None, device=None):
-#     return StructackBothEnds(degree_percentile_range=[0,.1,0,.1])
-
-def build_structack2_greedy(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackDegreeRandomLinking()
-
-def build_structack_fold(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackDegree()
-
-def build_structack_distance(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackDegreeDistance()
-
-def build_structack_only_distance(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackDistance()
-
-def build_structack_eigenvector_centrality(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackEigenvectorCentrality()
-
-def build_structack_betweenness_centrality(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackBetweennessCentrality()
-
-def build_structack_closeness_centrality(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackClosenessCentrality()
-
-def build_structack_pagerank(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackPageRank()
-
-def build_structack_katz_similarity(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackKatzSimilarity()
-
-def build_structack_community(adj=None, features=None, labels=None, idx_train=None, device=None):
-    return StructackCommunity()
 
 def build_mettack(adj=None, features=None, labels=None, idx_train=None, device=None):    
     lambda_ = 0
@@ -370,7 +269,7 @@ def main(datasets, model):
     test_func = func[model]
     df_path = f'reports/eval/baseline_eval-{model}.csv'
     attacks = [
-        # [attack_random, 'Random', build_random],
+        [attack_random, 'Random', build_random],
         [attack_dice, 'DICE', build_dice],
         [attack_mettaack, 'Metattack', build_mettack],
         [attack_pgd, 'PGD', build_pgd],
@@ -465,31 +364,6 @@ def combination(datasets, model):
     for selection, selection_name in selection_options:
         for connection, connection_name in connection_options:
             for dataset in datasets:
-                ''' Clean graph evaluation '''
-                # for split_seed in range(split_seeds):
-                #     np.random.seed(split_seed)
-                #     torch.manual_seed(split_seed)
-                #     if cuda:
-                #         torch.cuda.manual_seed(split_seed)
-                #     # reload the dataset with a different split (WARNING: this doesn't work for attack methods which depend on the split)
-                #     data = Dataset(root='/tmp/', name=dataset)
-                #     for seed in range(gcn_seeds):
-
-                #         np.random.seed(seed)
-                #         torch.manual_seed(seed)
-                #         if cuda:
-                #             torch.cuda.manual_seed(seed)
-                #         acc = test_gcn(postprocess_adj(data.adj).to(torch.device("cuda" if cuda else "cpu")),
-                #                     data, cuda, pre_test_data)
-                #         row = {'dataset':dataset, 'selection':'clean', 'connection':'clean',
-                #                 'gcn_seed':seed, 'acc':acc, 'perturbation_rate':0,'elapsed':0,
-                #                 'split_seed':split_seed}
-                #         print(row)
-                #         cdf = pd.DataFrame()
-                #         if os.path.exists(df_path):
-                #             cdf = pd.read_csv(df_path)
-                #         cdf = cdf.append(row, ignore_index=True)
-                #         cdf.to_csv(df_path,index=False)
 
                 data = Dataset(root='/tmp/', name=dataset)
                 print(f'attack [{selection_name}]*[{connection_name}]')
@@ -526,7 +400,7 @@ def combination(datasets, model):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Run structack.")
+    parser = argparse.ArgumentParser(description="Run GNN model on (perturbed) graphs.")
     parser.add_argument('--datasets', nargs='+', default=['citeseer', 'cora', 'cora_ml', 'polblogs', 'pubmed'], help='List of datasets to evaluate.')
     # parser.add_argument('--output', nargs='?', default='reports/eval/comb_acc_eval_noticeability.csv', help='Evaluation results output filepath.')
     parser.add_argument('--approach_type', nargs='?', default='structack', help='Type of approaches to run [baseline/structack/clean].')
@@ -549,58 +423,4 @@ if __name__ == '__main__':
 
 
 
-
-
-# The following lists should be correspondent
-
-
-
-# attacks = [
-#     attack_random,
-#     attack_dice,
-#     # attack_structack_fold, 
-#     # attack_structack_only_distance,
-#     # attack_structack_distance,
-#     attack_mettaack,
-#     attack_pgd,
-#     attack_minmax,
-#     # attack_structack,
-#     # attack_structack, 
-#     # attack_structack, 
-#     # attack_structack,
-#     # attack_structack,
-#     # attack_structack,
-# ]
-# model_names = [
-#     'Random',
-#     'DICE',
-#     # 'StructackGreedyFold', # this is StructackDegree in the paper
-#     # 'StructackOnlyDistance', # this is StructackDistance in the paper
-#     # 'StructackDistance', # this is Structack in the paper
-#     'Metattack',
-#     'PGD',
-#     'MinMax',
-#     # 'StructackEigenvectorCentrality',
-#     # 'StructackBetweennessCentrality',
-#     # 'StructackClosenessCentrality',
-#     # 'StructackPageRank',
-#     # 'StructackKatzSimilarity',
-#     # 'StructackCommunity',
-# ]
-# model_builders = [
-#     build_random,
-#     build_dice,
-#     # build_structack_fold,
-#     # build_structack_only_distance,
-#     # build_structack_distance,
-#     build_mettack,
-#     build_pgd,
-#     build_minmax,
-#     # build_structack_eigenvector_centrality, 
-#     # build_structack_betweenness_centrality, 
-#     # build_structack_closeness_centrality, 
-#     # build_structack_pagerank,
-#     # build_structack_katz_similarity,
-#     # build_structack_community,
-# ]
 
